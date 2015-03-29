872ab83b6ecff66e0df0e2ba5c53ccfc4a66d068
Stanislav Chekalin <stanislav.chekalin@gmail.com>
2015-03-29 07:58:17 +0100

    Expression evaluation: basic structure for lexer and parser

---
+++ src/parse.js
@@ -0,0 +1,59 @@
+/* jshint globalstrict: true */
+'use strict';
+
+function parse(expr) {
+    return new Parser(new Lexer()).parse(expr);
+}
+
+function Lexer() {
+
+}
+
+Lexer.prototype.lex = function (text) {
+    this.text = text;
+    this.index = 0;
+    this.ch = undefined;
+    this.tokens = [];
+
+    while (this.index < this.text.length) {
+        this.ch = this.text.charAt(this.index);
+        if (this.isNumber(this.ch)) {
+            this.readNumber();
+        } else {
+            throw 'Unexpected next character: ' + this.ch;
+        }
+    }
+
+    return this.tokens;
+};
+
+Lexer.prototype.isNumber = function (ch) {
+    return '0' <= ch && ch <= '9';
+};
+
+Lexer.prototype.readNumber = function () {
+    var number = '';
+    while (this.index < this.text.length) {
+        var ch = this.text.charAt(this.index);
+        if (this.isNumber(ch)) {
+            number += ch;
+        } else {
+            break;
+        }
+        this.index++;
+    }
+    number = 1 * number;
+    this.tokens.push({
+        text: number,
+        fn: _.constant(number)
+    });
+};
+
+function Parser(lexer) {
+    this.lexer = lexer;
+}
+
+Parser.prototype.parse = function (text) {
+    this.tokens = this.lexer.lex(text);
+    return _.first(this.tokens).fn;
+};
