4ca83370860cf14a6588ed7a21e5ed1447c4bec0
Stanislav Chekalin <stanislav.chekalin@gmail.com>
2015-03-31 08:55:08 +0100

    Expression evaluation: parsing arrays

--- test/parse.spec.js
+++ test/parse.spec.js
@@ -129,4 +129,25 @@ describe("parse", function () {
         var fn = parse(' \n42 ');
         expect(fn()).toBe(42);
     });
+
+    it("will parse an empty array", function () {
+        var fn = parse('[]');
+        expect(fn()).toEqual([]);
+    });
+
+    it("will parse a non-empty array", function () {
+        var fn = parse('[1, "two",[3]]');
+        expect(fn()).toEqual([1, 'two', [3]]);
+    });
+
+    it("will parse an array with trailing commas", function () {
+        var fn = parse('[1, 2, 3, ]');
+        expect(fn()).toEqual([1, 2, 3]);
+    });
+
+    it("marks array literals as literal and constant", function () {
+        var fn = parse('[1, 2, 3]');
+        expect(fn.literal).toBe(true);
+        expect(fn.constant).toBe(true);
+    });
 });
