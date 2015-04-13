f00718ce9eea6b3851f7c214c427b90c2433623d
Stanislav Chekalin <stanislav.chekalin@gmail.com>
2015-04-13 08:25:04 +0100

    base for module loader

---
+++ test/loader.spec.js
@@ -0,0 +1,36 @@
+/* jshint globalstrict: true */
+/* global setupModuleLoader: false */
+/* global it, expect, describe, beforeEach: false */
+
+'use strict';
+
+describe('setupModuleLoader', function () {
+
+    beforeEach(function () {
+        delete window.angular;
+    });
+
+    it('exposes angular in the window', function () {
+        setupModuleLoader(window);
+        expect(window.angular).toBeDefined();
+    });
+
+    it('creates angular just once', function () {
+        setupModuleLoader(window);
+        var ng = window.angular;
+        setupModuleLoader(window);
+        expect(window.angular).toBe(ng);
+    });
+
+    it('exposes the angular module function', function () {
+        setupModuleLoader(window);
+        expect(window.angular.module).toBeDefined();
+    });
+
+    it('exposes the angular module function just once', function () {
+        setupModuleLoader(window);
+        var module = window.angular.module;
+        setupModuleLoader(window);
+        expect(window.angular.module).toBe(module);
+    });
+});
