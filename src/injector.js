59a23a53364941602d9511592bd836fbcfe56dee
Stanislav Chekalin <stanislav.chekalin@gmail.com>
2015-04-25 12:36:37 +0100

    factories

--- src/injector.js
+++ src/injector.js
@@ -20,6 +20,17 @@ function createInjector(modulesToLoad, strictDi) {
     var loadedModules = new HashMap();
     var path = [];
     strictDi = (strictDi === true);
+
+    function enforceReturnValue(factoryFn) {
+        return function() {
+            var value = instanceInjector.invoke(factoryFn);
+            if (_.isUndefined(value)) {
+                throw 'factory must return a value';
+            }
+            return value;
+        };
+    }
+
     providerCache.$provide = {
         constant: function (key, value) {
             if (key === 'hasOwnProperty') {
@@ -33,6 +44,9 @@ function createInjector(modulesToLoad, strictDi) {
                 provider = providerInjector.instantiate(provider);
             }
             providerCache[key + 'Provider'] = provider;
+        },
+        factory: function (key, factoryFn) {
+            this.provider(key, {$get: enforceReturnValue(factoryFn)});
         }
     };
 
