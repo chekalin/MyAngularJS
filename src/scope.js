25f54ff3f496f185627df97a7900cfc147663bc5
Stanislav Chekalin <stanislav.chekalin@gmail.com>
2015-04-30 14:48:37 +0100

    TTL is configurable in root scope provider

--- src/scope.js
+++ src/scope.js
@@ -2,6 +2,15 @@
 "use strict";
 
 function $RootScopeProvider() {
+    var TTL = 10;
+
+    this.digestTtl = function (value) {
+        if (_.isNumber(value)) {
+            TTL = value;
+        }
+        return TTL;
+    };
+
     this.$get = ['$parse', function ($parse) {
 
         function initWatchVal() {
@@ -79,7 +88,7 @@ function $RootScopeProvider() {
         };
 
         Scope.prototype.$digest = function () {
-            var ttl = 10;
+            var ttl = TTL;
             var dirty;
             this.$root.$$lastDirtyWatch = null;
             this.$beginPhase("$digest");
@@ -101,7 +110,7 @@ function $RootScopeProvider() {
                 dirty = this.$$digestOnce();
                 if ((dirty || this.$$asyncQueue.length) && !(ttl--)) {
                     this.$clearPhase();
-                    throw "10 digest iterations reached";
+                    throw TTL + " digest iterations reached";
                 }
             } while (dirty || this.$$asyncQueue.length);
             this.$clearPhase();
