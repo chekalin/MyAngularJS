64ea4057f7da35402890bdbd7bdb505b87231102
Stanislav Chekalin <stanislav.chekalin@gmail.com>
2015-04-30 18:16:51 +0100

    $q: handling rejections

--- src/q.js
+++ src/q.js
@@ -5,8 +5,11 @@ function $QProvider() {
         function processQueue(state) {
             var pending = state.pending;
             delete state.pending;
-            _.forEach(pending, function (onFulfilled) {
-                onFulfilled(state.value);
+            _.forEach(pending, function (handlers) {
+                var fn = handlers[state.status];
+                if (_.isFunction(fn)) {
+                    fn(state.value);
+                }
             });
         }
 
@@ -18,12 +21,15 @@ function $QProvider() {
 
         function Promise() {
             this.$$state = {};
-            this.then = function (onFulfilled) {
+            this.then = function (onFulfilled, onRejected) {
                 this.$$state.pending = this.$$state.pending || [];
-                this.$$state.pending.push(onFulfilled);
+                this.$$state.pending.push([null, onFulfilled, onRejected]);
                 if (this.$$state.status > 0) {
                     scheduleProcessQueue(this.$$state);
                 }
+            };
+            this.catch = function (onRejected) {
+                this.then(null, onRejected);
             };
         }
 
@@ -36,6 +42,15 @@ function $QProvider() {
                 }
                 this.promise.$$state.value = v;
                 this.promise.$$state.status = 1;
+                scheduleProcessQueue(this.promise.$$state);
+            };
+
+            this.reject = function (v) {
+                if (this.promise.$$state.status) {
+                    return;
+                }
+                this.promise.$$state.value = v;
+                this.promise.$$state.status = 2;
                 scheduleProcessQueue(this.promise.$$state);
             };
         }
