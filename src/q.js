526e5c883fef1b16663f523fd85fdd1bf53a0965
chekalin <stanislav.chekalin@gmail.com>
2015-05-10 10:29:50 +0100

    $q: ES6 style promises, $$q does not depend on digest cycle

--- src/q.js
+++ src/q.js
@@ -1,172 +1,199 @@
+function qFactory(callLater) {
+    'use strict';
+
+    function makePromise(value, resolved) {
+        var d = new Deferred();
+        if (resolved) {
+            d.resolve(value);
+        } else {
+            d.reject(value);
+        }
+        return d.promise;
+    }
+
+    function handleFinallyCallback(callback, value, resolved) {
+        var callbackValue = callback();
+        if (callbackValue && callbackValue.then) {
+            return callbackValue.then(function () {
+                return makePromise(value, resolved);
+            });
+        } else {
+            return makePromise(value, resolved);
+        }
+    }
+
+    function processQueue(state) {
+        var pending = state.pending;
+        delete state.pending;
+        _.forEach(pending, function (handlers) {
+            var deferred = handlers[0];
+            var fn = handlers[state.status];
+            try {
+                if (_.isFunction(fn)) {
+                    deferred.resolve(fn(state.value));
+                } else if (state.status === 1) {
+                    deferred.resolve(state.value);
+                } else {
+                    deferred.reject(state.value);
+                }
+            } catch (e) {
+                deferred.reject(e);
+            }
+        });
+    }
+
+    function scheduleProcessQueue(state) {
+        callLater(function () {
+            processQueue(state);
+        });
+    }
+
+    function Promise() {
+        this.$$state = {};
+        this.then = function (onFulfilled, onRejected, onProgress) {
+            var result = new Deferred();
+            this.$$state.pending = this.$$state.pending || [];
+            this.$$state.pending.push([result, onFulfilled, onRejected, onProgress]);
+            if (this.$$state.status > 0) {
+                scheduleProcessQueue(this.$$state);
+            }
+            return result.promise;
+        };
+        this.catch = function (onRejected) {
+            return this.then(null, onRejected);
+        };
+        this.finally = function (callback, progressBack) {
+            return this.then(function (value) {
+                    return handleFinallyCallback(callback, value, true);
+                }, function (rejection) {
+                    return handleFinallyCallback(callback, rejection, false);
+                },
+                progressBack
+            );
+        };
+    }
+
+    function Deferred() {
+        this.promise = new Promise();
+
+        this.resolve = function (value) {
+            if (this.promise.$$state.status) {
+                return;
+            }
+            if (value && _.isFunction(value.then)) {
+                value.then(
+                    _.bind(this.resolve, this),
+                    _.bind(this.reject, this),
+                    _.bind(this.notify, this)
+                );
+            } else {
+                this.promise.$$state.value = value;
+                this.promise.$$state.status = 1;
+                scheduleProcessQueue(this.promise.$$state);
+            }
+        };
+
+        this.reject = function (v) {
+            if (this.promise.$$state.status) {
+                return;
+            }
+            this.promise.$$state.value = v;
+            this.promise.$$state.status = 2;
+            scheduleProcessQueue(this.promise.$$state);
+        };
+
+        this.notify = function (progress) {
+            var pending = this.promise.$$state.pending;
+            if (pending && pending.length && !this.promise.$$state.status) {
+                callLater(function () {
+                    _.forEach(pending, function (handlers) {
+                        var deferred = handlers[0];
+                        var progressCallback = handlers[3];
+                        try {
+                            deferred.notify(_.isFunction(progressCallback) ?
+                                progressCallback(progress) :
+                                progress);
+                        } catch (e) {
+                            console.log(e);
+                        }
+                    });
+                });
+            }
+        };
+    }
+
+    function defer() {
+        return new Deferred();
+    }
+
+    function reject(rejection) {
+        var d = defer();
+        d.reject(rejection);
+        return d.promise;
+    }
+
+    function when(value, callback, errback, progressback) {
+        var d = defer();
+        d.resolve(value);
+        return d.promise.then(callback, errback, progressback);
+    }
+
+    function all(promises) {
+        var results = _.isArray(promises) ? [] : {};
+        var counter = 0;
+        var d = defer();
+        _.forEach(promises, function (promise, index) {
+            counter++;
+            when(promise).then(function (value) {
+                results[index] = value;
+                counter--;
+                if (!counter) {
+                    d.resolve(results);
+                }
+            }, function (rejection) {
+                d.reject(rejection);
+            });
+        });
+        if (!counter) {
+            d.resolve(results);
+        }
+        return d.promise;
+    }
+
+    var $Q = function Q(resolver) {
+        if (!_.isFunction(resolver)) {
+            throw 'Expected function, got ' + resolver;
+        }
+        var d = defer();
+        resolver(_.bind(d.resolve, d), _.bind(d.reject, d));
+        return d.promise;
+    };
+
+    return _.extend($Q, {
+        defer: defer,
+        reject: reject,
+        when: when,
+        all: all
+    });
+
+}
+
 function $QProvider() {
     'use strict';
 
     this.$get = ['$rootScope', function ($rootScope) {
-        function makePromise(value, resolved) {
-            var d = new Deferred();
-            if (resolved) {
-                d.resolve(value);
-            } else {
-                d.reject(value);
-            }
-            return d.promise;
-        }
-
-        function handleFinallyCallback(callback, value, resolved) {
-            var callbackValue = callback();
-            if (callbackValue && callbackValue.then) {
-                return callbackValue.then(function () {
-                    return makePromise(value, resolved);
-                });
-            } else {
-                return makePromise(value, resolved);
-            }
-        }
-
-        function processQueue(state) {
-            var pending = state.pending;
-            delete state.pending;
-            _.forEach(pending, function (handlers) {
-                var deferred = handlers[0];
-                var fn = handlers[state.status];
-                try {
-                    if (_.isFunction(fn)) {
-                        deferred.resolve(fn(state.value));
-                    } else if (state.status === 1) {
-                        deferred.resolve(state.value);
-                    } else {
-                        deferred.reject(state.value);
-                    }
-                } catch (e) {
-                    deferred.reject(e);
-                }
-            });
-        }
-
-        function scheduleProcessQueue(state) {
-            $rootScope.$evalAsync(function () {
-                processQueue(state);
-            });
-        }
-
-        function Promise() {
-            this.$$state = {};
-            this.then = function (onFulfilled, onRejected, onProgress) {
-                var result = new Deferred();
-                this.$$state.pending = this.$$state.pending || [];
-                this.$$state.pending.push([result, onFulfilled, onRejected, onProgress]);
-                if (this.$$state.status > 0) {
-                    scheduleProcessQueue(this.$$state);
-                }
-                return result.promise;
-            };
-            this.catch = function (onRejected) {
-                return this.then(null, onRejected);
-            };
-            this.finally = function (callback, progressBack) {
-                return this.then(function (value) {
-                        return handleFinallyCallback(callback, value, true);
-                    }, function (rejection) {
-                        return handleFinallyCallback(callback, rejection, false);
-                    },
-                    progressBack
-                );
-            };
-        }
-
-        function Deferred() {
-            this.promise = new Promise();
-
-            this.resolve = function (value) {
-                if (this.promise.$$state.status) {
-                    return;
-                }
-                if (value && _.isFunction(value.then)) {
-                    value.then(
-                        _.bind(this.resolve, this),
-                        _.bind(this.reject, this),
-                        _.bind(this.notify, this)
-                    );
-                } else {
-                    this.promise.$$state.value = value;
-                    this.promise.$$state.status = 1;
-                    scheduleProcessQueue(this.promise.$$state);
-                }
-            };
-
-            this.reject = function (v) {
-                if (this.promise.$$state.status) {
-                    return;
-                }
-                this.promise.$$state.value = v;
-                this.promise.$$state.status = 2;
-                scheduleProcessQueue(this.promise.$$state);
-            };
-
-            this.notify = function (progress) {
-                var pending = this.promise.$$state.pending;
-                if (pending && pending.length && !this.promise.$$state.status) {
-                    $rootScope.$evalAsync(function () {
-                        _.forEach(pending, function (handlers) {
-                            var deferred = handlers[0];
-                            var progressCallback = handlers[3];
-                            try {
-                                deferred.notify(_.isFunction(progressCallback) ?
-                                    progressCallback(progress) :
-                                    progress);
-                            } catch (e) {
-                                console.log(e);
-                            }
-                        });
-                    });
-                }
-            };
-        }
-
-        function defer() {
-            return new Deferred();
-        }
-
-        function reject(rejection) {
-            var d = defer();
-            d.reject(rejection);
-            return d.promise;
-        }
-
-        function when(value, callback, errback, progressback) {
-            var d = defer();
-            d.resolve(value);
-            return d.promise.then(callback, errback, progressback);
-        }
-
-        function all(promises) {
-            var results = _.isArray(promises) ? [] : {};
-            var counter = 0;
-            var d = defer();
-            _.forEach(promises, function (promise, index) {
-                counter++;
-                when(promise).then(function (value) {
-                    results[index] = value;
-                    counter--;
-                    if (!counter) {
-                        d.resolve(results);
-                    }
-                }, function (rejection) {
-                    d.reject(rejection);
-                });
-            });
-            if (!counter) {
-                d.resolve(results);
-            }
-            return d.promise;
-        }
-
-        return {
-            defer: defer,
-            reject: reject,
-            when: when,
-            all: all
-        };
+        return qFactory(function (callback) {
+            $rootScope.$evalAsync(callback);
+        });
     }];
+}
 
+function $$QProvider() {
+    'use strict';
+
+    this.$get = function () {
+        return qFactory(function (callback) {
+            setTimeout(callback, 0);
+        });
+    };
 }
--- test/q.spec.js
+++ test/q.spec.js
@@ -1,13 +1,14 @@
-/* global it, expect, describe, beforeEach: false */
+/* global it, expect, describe, beforeEach, afterEach: false */
 /* global publishExternalAPI, createInjector: false */
 
 describe('$q', function () {
-    var $q, $rootScope;
+    var $q, $$q, $rootScope;
 
     beforeEach(function () {
         publishExternalAPI();
         var injector = createInjector(['ng']);
         $q = injector.get('$q');
+        $$q = injector.get('$$q');
         $rootScope = injector.get('$rootScope');
     });
 
@@ -762,6 +763,97 @@ describe('$q', function () {
             $rootScope.$apply();
 
             expect(fulfilledSpy).toHaveBeenCalledWith([1, 2, 3]);
+        });
+
+    });
+
+    describe('ES6 style', function () {
+
+        it('is a function', function () {
+            expect($q instanceof Function).toBe(true);
+        });
+
+        it('expects a function as an argument', function () {
+            expect($q).toThrow();
+            $q(_.noop); // Just checking that this doesn't throw
+        });
+
+        it('returns a promise', function () {
+            expect($q(_.noop)).toBeDefined();
+            expect($q(_.noop).then).toBeDefined();
+        });
+
+        it('calls function with a resolve function', function () {
+            var fulfilledSpy = jasmine.createSpy();
+
+            $q(function (resolve) {
+                resolve('ok');
+            }).then(fulfilledSpy);
+
+            $rootScope.$apply();
+
+            expect(fulfilledSpy).toHaveBeenCalledWith('ok');
+        });
+
+        it('calls function with a reject function', function () {
+            var fulfilledSpy = jasmine.createSpy();
+            var rejectedSpy = jasmine.createSpy();
+
+            $q(function (resolve, reject) {
+                reject('fail');
+            }).then(fulfilledSpy, rejectedSpy);
+
+            $rootScope.$apply();
+
+            expect(fulfilledSpy).not.toHaveBeenCalled();
+            expect(rejectedSpy).toHaveBeenCalledWith('fail');
+        });
+
+    });
+
+    describe('$$q', function () {
+
+        beforeEach(function () {
+            jasmine.clock().install();
+        });
+
+        afterEach(function () {
+            jasmine.clock().uninstall();
+        });
+
+        it('uses deferreds that do not resolve at digest', function () {
+            var d = $$q.defer();
+            var fulfilledSpy = jasmine.createSpy();
+            d.promise.then(fulfilledSpy);
+            d.resolve('ok');
+
+            $rootScope.$apply();
+
+            expect(fulfilledSpy).not.toHaveBeenCalled();
+        });
+
+        it('uses deferreds that resolve later', function () {
+            var d = $$q.defer();
+            var fulfilledSpy = jasmine.createSpy();
+            d.promise.then(fulfilledSpy);
+            d.resolve('ok');
+
+            jasmine.clock().tick(1);
+
+            expect(fulfilledSpy).toHaveBeenCalledWith('ok');
+        });
+
+        it('does not invoke digest', function () {
+            var d = $$q.defer();
+            d.promise.then(_.noop);
+            d.resolve('ok');
+
+            var watchSpy = jasmine.createSpy();
+            $rootScope.$watch(watchSpy);
+
+            jasmine.clock().tick(1);
+
+            expect(watchSpy).not.toHaveBeenCalled();
         });
 
     });
