4c0263d1d5394eb81ea0c8c45e7ab6a8a5527af6
chekalin <stanislav.chekalin@gmail.com>
2015-05-24 07:52:45 +0100

    $http: JSON serialization

--- src/http.js
+++ src/http.js
@@ -1,6 +1,37 @@
 function $HttpProvider() {
     'use strict';
 
+    function isBlob(object) {
+        return object.toString() === '[object Blob]';
+    }
+
+    function isFile(object) {
+        return object.toString() === '[object File]';
+    }
+
+    function isFormData(object) {
+        return object.toString() === '[object FormData]';
+    }
+
+    function isJsonLike(data) {
+        if (data.match(/^\{(?!\{)/)) {
+            return data.match(/\}$/);
+        } else if (data.match(/^\[/)) {
+            return data.match(/\]$/);
+        }
+    }
+
+    function defaultHttpResponseTransform(data, headers) {
+        if (_.isString(data)) {
+            var contentType = headers('Content-Type');
+            if ((contentType && contentType.indexOf('application/json') === 0) ||
+                isJsonLike(data)) {
+                return JSON.parse(data);
+            }
+        }
+        return data;
+    }
+
     var defaults = this.defaults = {
         headers: {
             common: {
@@ -15,7 +46,15 @@ function $HttpProvider() {
             patch: {
                 'Content-Type': 'application/json;charset=utf-8'
             }
-        }
+        },
+        transformRequest: [function (data) {
+            if (_.isObject(data) && !isBlob(data) && !isFile(data) && !isFormData(data)) {
+                return JSON.stringify(data);
+            } else {
+                return data;
+            }
+        }],
+        transformResponse: [defaultHttpResponseTransform]
     };
 
     function executeHeaderFns(headers, config) {
