cb7096e977c55bedb2fc27241a1212b3c728b00f
chekalin <stanislav.chekalin@gmail.com>
2015-08-09 11:52:10 +0100

    Transclusion from Descendant Nodes

--- src/compile.js
+++ src/compile.js
@@ -555,6 +555,8 @@ function $CompileProvider($provide) {
                         return boundTranscludeFn(transcludedScope, scope);
                     }
 
+                    scopeBoundTranscludeFn.$$boundTransclude = boundTranscludeFn;
+
                     _.forEach(preLinkFns, function (linkFn) {
                         linkFn(
                             linkFn.isolateScope ? isolateScope : scope,
@@ -569,7 +571,7 @@ function $CompileProvider($provide) {
                         if (newIsolateScopeDirective && newIsolateScopeDirective.template) {
                             scopeToChild = isolateScope;
                         }
-                        childLinkFn(scopeToChild, linkNode.childNodes);
+                        childLinkFn(scopeToChild, linkNode.childNodes, boundTranscludeFn);
                     }
                     _.forEachRight(postLinkFns, function (linkFn) {
                         linkFn(
@@ -613,7 +615,7 @@ function $CompileProvider($provide) {
                         });
                     }
                 });
-                function compositeLinkFn(scope, linkNodes) {
+                function compositeLinkFn(scope, linkNodes, parentBoundTranscludeFn) {
                     var stableNodeList = [];
                     _.forEach(linkFns, function (linkFn) {
                         var nodeIdx = linkFn.idx;
@@ -637,6 +639,8 @@ function $CompileProvider($provide) {
                                     }
                                     return linkFn.nodeLinkFn.transclude(transcludedScope);
                                 };
+                            } else if (parentBoundTranscludeFn) {
+                                boundTranscludeFn = parentBoundTranscludeFn;
                             }
                             linkFn.nodeLinkFn(
                                 linkFn.childLinkFn,
@@ -647,7 +651,8 @@ function $CompileProvider($provide) {
                         } else {
                             linkFn.childLinkFn(
                                 scope,
-                                node.childNodes
+                                node.childNodes,
+                                parentBoundTranscludeFn
                             );
                         }
                     });
@@ -659,9 +664,14 @@ function $CompileProvider($provide) {
 
             function compile($compileNodes) {
                 var compositeLinkFn = compileNodes($compileNodes);
-                return function publicLinkFn(scope) {
+                return function publicLinkFn(scope, options) {
+                    options = options || {};
+                    var parentBoundTranscludeFn = options.parentBoundTranscludeFn;
+                    if (parentBoundTranscludeFn && parentBoundTranscludeFn.$$boundTransclude) {
+                        parentBoundTranscludeFn = parentBoundTranscludeFn.$$boundTransclude;
+                    }
                     $compileNodes.data('$scope', scope);
-                    compositeLinkFn(scope, $compileNodes);
+                    compositeLinkFn(scope, $compileNodes, parentBoundTranscludeFn);
                     return $compileNodes;
                 };
             }
